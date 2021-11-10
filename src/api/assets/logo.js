31d416bb66ee8e739d6c9e9efdc3fedf1777c3bb
fpunny <frederic.pun@gmail.com>
2021-11-10 10:22:53 -0500

    owo

--- src/api/assets/logo.js
+++ src/api/assets/logo.js
@@ -1,7 +1,7 @@
 import content from '!raw-loader!./logo.svg';
 
 export default async function handler(req, res) {
-  res.set('Content-Type', 'image/svg+xml');
+  res.setHeader('Content-Type', 'image/svg+xml');
   res
     .status(200)
     .send(content.replace('{{COLOR}}', req.query.color ?? 'currentColor'));
